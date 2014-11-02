2aecdb603d729b9a089ca5cd60ee36699a726079
ulrohn <c12jvr@cs.umu.se>
2014-11-02 08:37:57 +0100

    running and kicking animation

---
+++ Assets/Fighter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fighter : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = (Animator)GetComponent("Animator");
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.LeftShift)) {
+			print("here");
+			anim.SetInteger("kick", 1);
+		} else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+			anim.SetInteger("kick", 0);
+		}
+
+		if (Input.GetKey(KeyCode.D)) {
+			anim.SetInteger("running", 1);
+		} else {
+			anim.SetInteger("running", 0);
+		}
+	}	
+}
