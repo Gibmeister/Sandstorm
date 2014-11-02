8bc8897faa2c8e6ef58efee1220bfaad780e76e3
Joel Persson <joelpersso@gmail.com>
2014-11-02 09:05:42 +0100

    Now able to move on GameObject ROAD

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -9,27 +9,28 @@ public class Test : MonoBehaviour
 
 
 
-Vector2 jumpForce = new Vector2 (0, 2);
-DistanceJoint2D distJoint ;
 bool falling = false;
 Transform shadow;
 Transform oldShadow;
 static float  jump = 0;
-Vector3 pos;
 // Use this for initialization
 void Start ()
 {
 		shadow = GameObject.Find ("Shadow").transform;
 		oldShadow = shadow;
-		//transform.position = new Vector3 (shadow.position.x, shadow.position.y, shadow.position.z ) ; 
 }
 // Update is called once per frame
 void Update ()
 {
+	if (transform.position.y > oldShadow.transform.position.y) {
+		//Updating new position.
+		float yPosi = shadow.transform.position.y + jump;
+		transform.position = new Vector3 (shadow.transform.position.x, yPosi, transform.position.z);
+	} else {
+		transform.position = new Vector3 (shadow.transform.position.x, shadow.transform.position.y, transform.position.z);
+	}
 	if (Input.GetKey ("space")) {
-			print("gonna jump");
-			print("a: " + (Mathf.Abs(transform.position.y-oldShadow.position.y)<maxJumpHeight));
-			if(!falling && Mathf.Abs(transform.position.y-oldShadow.position.y)<maxJumpHeight) {
+		if(!falling && Mathf.Abs(transform.position.y-oldShadow.position.y)<maxJumpHeight ) {
 			Jump();
 		} else {
 			Fall();
@@ -37,60 +38,28 @@ void Update ()
 	} else {
 		Fall();
 	}
-
-		if (transform.position.y > oldShadow.transform.position.y) {
-						//Updating new position.
-						float yPosi = shadow.transform.position.y + Mathf.Abs (transform.position.y - oldShadow.transform.position.y);
-						transform.position = new Vector3 (shadow.transform.position.x, yPosi, transform.position.z);
-		
-		} else {
-			transform.position = new Vector3 (shadow.transform.position.x, shadow.transform.position.y, transform.position.z);
-					
-
-				}
-				
-					//transform.position = new Vector3 (shadow.position.x + jump, 
-  				    //                              shadow.position.y + jump,
-  				    //                              transform.position.z - jump);
-
-
-	if (transform.position.y > shadow.position.y) {
-				
-	}
-
-
-//				transform.position = new Vector3 (shadow.position.x, shadow.position.y, shadow.position.z);
-//				pos = new Vector3 (shadow.position.x + jump, 
-//			                    shadow.position.y + jump,
-//			                    transform.position.z - jump);
-//				if (shadow.position.x < pos.x) {
-//						transform.position = new Vector3 (shadow.position.x + jump, 
-//				                                  shadow.position.y + jump,
-//				                                  transform.position.z - jump);
-//				}
-
-		oldShadow = shadow;
-	}
-	void Jump(){
-	//	jump += .01f;
+	oldShadow = shadow;
+}
+void Jump(){
+	jump += jumpSpeed;
+	transform.position = new Vector3 (transform.position.x, 
+	                                  shadow.transform.position.y + jump,
+	                                  transform.position.z);
+}
+void Fall() {
+	if (transform.position.y > oldShadow.transform.position.y) {
+		jump -= fallSpeed;
+		falling = true;
 		transform.position = new Vector3 (transform.position.x, 
-		                                  transform.position.y + jumpSpeed,
-		                                  transform.position.z);
+	                         shadow.transform.position.y + jump,
+	                         transform.position.z);
+	} else if (transform.position.y != oldShadow.transform.position.y) {
+		falling = false;
+		transform.position = new Vector3 (transform.position.x, 
+	                         oldShadow.transform.position.y,
+	                         transform.position.z);
+	} else {
+		falling = false;
 	}
-	void Fall() {
-		if (transform.position.y > oldShadow.transform.position.y) {
-			//jump -= 0.2f;
-			falling = true;
-			transform.position = new Vector3 (transform.position.x, 
-		                         transform.position.y - fallSpeed,
-		                         transform.position.z);
-		} else if (transform.position.y != oldShadow.transform.position.y) {
-			falling = false;
-			transform.position = new Vector3 (transform.position.x, 
-		                         oldShadow.transform.position.y,
-		                         transform.position.z);
-		} else {
-			falling = false;
-		}
-	}
+}
 }
--- Assets/shadowMover.cs
+++ Assets/shadowMover.cs
@@ -3,17 +3,12 @@ using System.Collections;
 
 public class shadowMover : MonoBehaviour {
 
-	static float horizontalSpeed = 10;
-	Vector2 jumpForce = new Vector2(0,10);
-	Vector2 moveForce = new Vector2(horizontalSpeed,0);
-	static bool jumping = false;
+	static float horizontalSpeed = 0.1f;
 
 	static float yUpperBound;
 	static float yLowerBound;
 	static float xUpperBound;
 	static float xLowerBound;
-
-	static Vector3 minimsize = new Vector3(0.3f,0.3f,0.3f); 
 	// Use this for initialization
 	void Start () {
 
@@ -29,16 +24,16 @@ public class shadowMover : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.D) && !(transform.position.x >= xUpperBound)) {
-			transform.position = new Vector3 (transform.position.x+0.1f, transform.position.y, transform.position.z );
+			transform.position = new Vector3 (transform.position.x+horizontalSpeed, transform.position.y, transform.position.z );
 		}
 		if (Input.GetKey (KeyCode.A)&& !(transform.position.x <= xLowerBound)) {
-			transform.position = new Vector3 (transform.position.x-0.1f, transform.position.y, transform.position.z );
+			transform.position = new Vector3 (transform.position.x-horizontalSpeed, transform.position.y, transform.position.z );
 		}
 		if (Input.GetKey (KeyCode.W)&& !(transform.position.y >= yUpperBound)) {
-			transform.position = new Vector3 (transform.position.x, transform.position.y+0.1f, transform.position.z ); 
+			transform.position = new Vector3 (transform.position.x, transform.position.y+horizontalSpeed, transform.position.z ); 
 		}
 		if (Input.GetKey (KeyCode.S) && !(transform.position.y <= yLowerBound)) {
-			transform.position = new Vector3 (transform.position.x, transform.position.y-0.1f, transform.position.z ); 
+			transform.position = new Vector3 (transform.position.x, transform.position.y-horizontalSpeed, transform.position.z ); 
 		}
 	}
 }
