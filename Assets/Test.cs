b539fdf3a2355d06e04178b311b3c2f8793e73fb
ulrohn <c12jvr@cs.umu.se>
2014-11-02 03:52:38 +0100

    testing stuff

---
+++ Assets/Test.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+		Vector2 jumpForce = new Vector2 (0, 2);
+		DistanceJoint2D distJoint ;
+		bool jumping = false;
+		Transform shadow;
+		float  jump = 0;
+		// Use this for initialization
+		void Start ()
+		{
+				shadow = GameObject.Find ("Shadow").transform;
+
+				//distJoint = (DistanceJoint2D) gameObject.GetComponent("DistanceJoint2D");
+		}
+		// Update is called once per frame
+		void Update ()
+		{
+			if (Input.GetKey ("space")) {
+				Jump();
+			jumping = true;
+			} else if (jumping) {
+				jump = -1;
+			} else {
+				jump = 0;
+			jumping = false;
+				transform.position = new Vector3 (shadow.position.x + jump, 
+			                                  shadow.position.y + jump,
+			                                  transform.position.z - jump);
+			}
+		}
+
+		void Jump(){
+			jump = 1;
+	}
+}
---
+++ Assets/shadowMover.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shadowMover : MonoBehaviour {
+
+	static float horizontalSpeed = 10;
+	Vector2 jumpForce = new Vector2(0,10);
+	Vector2 moveForce = new Vector2(horizontalSpeed,0);
+	static float yUpperBound = 20;
+	static float yLowerBound = 3;
+	static float xLeftBound = -24;
+	static float xRightBound = 19;
+	static bool jumping = false;
+
+	static Vector3 minimsize = new Vector3(0.3f,0.3f,0.3f); 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.D)) {
+			rigidbody2D.AddForce(moveForce);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			rigidbody2D.AddForce(-moveForce);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			rigidbody2D.AddForce(jumpForce);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			rigidbody2D.AddForce(-jumpForce);
+		}
+		
+		if((transform.position.x <= xLeftBound) && rigidbody2D.velocity.x < 0){
+			rigidbody2D.velocity = new Vector2((rigidbody2D.velocity.x * -0.1f), rigidbody2D.velocity.y);
+		}
+		if((transform.position.x >= xRightBound) && rigidbody2D.velocity.x > 0){
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x * -0.1f, rigidbody2D.velocity.y);
+		}
+		
+		if((transform.position.y <= yLowerBound) && rigidbody2D.velocity.y < 0){
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y * -0.1f);
+		}
+		if((transform.position.y >= yUpperBound) && rigidbody2D.velocity.y > 0){
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y * -0.1f);
+		}
+	}
+}
