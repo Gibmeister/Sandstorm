f5258a1f97caac822e48c82a98009410ef4be891
ulrohn <c12jvr@cs.umu.se>
2014-11-04 08:54:03 +0100

    testing 3d stuff

---
+++ Assets/RunnerMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunnerMovement : MonoBehaviour {
+
+	Vector3 right;
+	Vector3 left;
+	Vector3 up;
+	Vector3 down;
+	Vector3 jump;
+	float speed = 5;
+	float jumpSpeed = 20;
+	bool isJumping = false;
+	bool facingLeft = false;
+
+	static float tau = 0.02f;
+
+	// Use this for initialization
+	void Start () {
+		right = new Vector3(speed,0,0);
+		left = new Vector3(-speed,0,0);
+		up = new Vector3(0,0,speed);
+		down = new Vector3(0,0,-speed);
+		jump = new Vector3(0,jumpSpeed,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.D)){
+			if (!facingLeft) {
+				transform.transform.Rotate(new Vector3(0,180,0));
+				facingLeft = true;
+			}
+			Move (right);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+
+			if (facingLeft) {
+				facingLeft = false;
+				transform.transform.Rotate(new Vector3(0,180,0));
+			}
+			Move(left);
+		}
+		if (Input.GetKey(KeyCode.W)) {
+			Move(up);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			Move(down);
+		}
+
+		if (Input.GetKey(KeyCode.Space)) {
+			Move(jump);
+
+		}
+
+	}
+
+	void Move(Vector3 direction) {
+		rigidbody.AddForce(direction);
+	}
+}
+
