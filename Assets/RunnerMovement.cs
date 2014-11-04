634fe1cef552ddcdcf49f4934d14f56f814fa6d4
ulrohn <c12jvr@cs.umu.se>
2014-11-04 09:18:03 +0100

    more 3D stuff in a 3D world

--- Assets/RunnerMovement.cs
+++ Assets/RunnerMovement.cs
@@ -8,11 +8,11 @@ public class RunnerMovement : MonoBehaviour {
 	Vector3 up;
 	Vector3 down;
 	Vector3 jump;
-	float speed = 5;
+	float speed = 8;
 	float jumpSpeed = 20;
 	bool isJumping = false;
-	bool facingLeft = false;
-
+	bool facingLeft = true;
+	Animator anim;
 	static float tau = 0.02f;
 
 	// Use this for initialization
@@ -22,6 +22,7 @@ public class RunnerMovement : MonoBehaviour {
 		up = new Vector3(0,0,speed);
 		down = new Vector3(0,0,-speed);
 		jump = new Vector3(0,jumpSpeed,0);
+		anim = (Animator)GetComponent("Animator");
 	}
 	
 	// Update is called once per frame
@@ -31,15 +32,18 @@ public class RunnerMovement : MonoBehaviour {
 				transform.transform.Rotate(new Vector3(0,180,0));
 				facingLeft = true;
 			}
+			anim.SetInteger("running", 1);
 			Move (right);
-		}
-		if (Input.GetKey(KeyCode.A)) {
-
+		}else if (Input.GetKey(KeyCode.A)) {
+			anim.SetInteger("running", 1);
 			if (facingLeft) {
 				facingLeft = false;
 				transform.transform.Rotate(new Vector3(0,180,0));
 			}
 			Move(left);
+		}
+		else { 
+			anim.SetInteger("running", 0);
 		}
 		if (Input.GetKey(KeyCode.W)) {
 			Move(up);
